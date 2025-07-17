73a74c1cb57ef790b3fe8bd848d64d1c8d5c7e87
Isha <isha18919@gmail.com>
2025-07-17 19:46:23 +0530

    update css

--- herosection/src/components/index.jsx
+++ herosection/src/components/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./hero.css";
-import bgImage from "../images/bg.png";
-// import arrowImage from "../images/Group 73.png";
+import bgImage from "../assets/bg.png";
+import arrowImage from "../assets/arrow.png";
 
 export function HeroSection() {
   return (
@@ -9,27 +9,35 @@ export function HeroSection() {
       <div className="overlay">
         <span className="welcome">WELCOME TO ALLSEE</span>
 
-        {/* h1 ma first text isha aama me css karyu bov kharab che e deit karj*/}
+        
         <h1 className="heading-row">
           <span className="heading-left"><span className="heading-premium">Premium</span><br /><span className="heading-solutions">Solutions</span></span>
+          <div className="description-row">
+          <p className="description">
+            Elevate your brand experience with Allsee's high-quality textiles — from custom clothes and uniforms to premium hotel and hospital linen. Trusted by industry leaders for comfort, durability, and timeless style.
+          </p>
+          <div className="hero-arrow">
+                <img src={arrowImage} alt="arrow"/>
+        </div>
+        </div>
           <span className="heading-right"><span className="heading-textile">Textile</span><br /><span className="heading-for-every">for every Industry</span></span>
         </h1>
 
         
-        {/* have react ma kai chage no karti khali css chane karj */}
-        <div className="description-row">
-          <p className="description">
-            Elevate your brand experience with Allsee's high-quality textiles — from custom clothes and uniforms to premium hotel and hospital linen. Trusted by industry leaders for comfort, durability, and timeless style.
-          </p>
-
-
-          <div className="line-circle">
-            {/* <div className="horizontal-line"></div>
-            <div className="circle">
-            </div> */}
-          </div>
-        </div>
+        
+        
       </div>
+      <div className="strip-container">
+  <div className="strip">
+    <span> New Collection 2025</span>
+    <span># Get Up to 50% off</span>
+    <span># Textiles</span>
+    <span># Clothing</span>
+    <span># Best Fashion Deals</span>
+    <span># Shop Now</span>
+  </div>
+</div>
+
     </section>
   );
 }
